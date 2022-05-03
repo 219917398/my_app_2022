f10d5e329ffd9a13e0f3de955c933bd8fd286ea0
Mia Villarreal <mia.villarreal@xembletech.com>
2022-05-03 12:04:52 -0500

    050322 - 12.04

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,93 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+  title: Text('TEXT widget'),
+),
+    body: Container(
+      margin: const EdgeInsets.all(10.0),
+      color: Colors.amber[600],
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+      child: Column(
+        children: [
+          Row(
+            children: const <Widget>[
+              Expanded(
+                child: Text('ROW widget', textAlign: TextAlign.center),
+              ),
+              Expanded(
+                child: Text('ROW widget', textAlign: TextAlign.center),
+              ),
+              Expanded(
+                child: Text('ROW widget', textAlign: TextAlign.center),
+              ),
+              Expanded(
+                child: Text('ROW widget', textAlign: TextAlign.center),
+              ),
+              Expanded(
+                child: Text('ROW widget', textAlign: TextAlign.center),
+              ),
+            ],
+          ),
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World UDG '),
-        ),
+          Row(
+            children: const <Widget>[
+              Expanded(
+                child: Text('COLUMN widget', textAlign: TextAlign.center),
+              ),
+              Expanded(
+                child: Text('COLUMN widget', textAlign: TextAlign.center),
+              ),
+              Expanded(
+                child: Text('COLUMN widget', textAlign: TextAlign.center),
+              ),
+              Expanded(
+                child: Text('COLUMN widget', textAlign: TextAlign.center),
+              ),
+              Expanded(
+                child: Text('COLUMN widget', textAlign: TextAlign.center),
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              Stack(
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 90,
+                    height: 90,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    width: 80,
+                    height: 80,
+                    color: Colors.blue,
+                    child: const Text('STACK widget'),
+                  ),
+                ],
+              ),
+            ],
+
+          ),
+
+        ],
       ),
-    );
-  }
-}
+
+
+      ),
+
+
+
+
+      ),
+    ),
+);
+
+
