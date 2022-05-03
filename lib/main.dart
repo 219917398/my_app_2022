364b89c86d550b02d97a4413aa574f216fd33997
Mia Villarreal <mia.villarreal@xembletech.com>
2022-05-03 14:49:48 -0500

    050322 - 14.49

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,51 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-  title: Text('TEXT widget'),
+  title: Text('Playlist Panda Movies'),
+      backgroundColor: Colors.pink[400] ,
 ),
     body: Container(
       margin: const EdgeInsets.all(10.0),
       color: Colors.amber[600],
 
-      child: Column(
-        children: [
-          Row(
-            children: const <Widget>[
-              Expanded(
-                child: Text('ROW widget', textAlign: TextAlign.center),
+      child: Stack(
+          children: const [
+          Center(
+            child: Image(image:NetworkImage('https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80')
+),
+            ),
+            Center(
+              child: Text(
+                  'Bienvenido a mi catálogo de películas.',
+                  style:TextStyle(
+                  color: Colors.white,
+                    backgroundColor: Colors.pink,
+                    fontSize: 40,
               ),
-              Expanded(
-                child: Text('ROW widget', textAlign: TextAlign.center),
               ),
-              Expanded(
-                child: Text('ROW widget', textAlign: TextAlign.center),
-              ),
-              Expanded(
-                child: Text('ROW widget', textAlign: TextAlign.center),
-              ),
-              Expanded(
-                child: Text('ROW widget', textAlign: TextAlign.center),
-              ),
-            ],
-          ),
+            ),
 
-          Row(
-            children: const <Widget>[
-              Expanded(
-                child: Text('COLUMN widget', textAlign: TextAlign.center),
-              ),
-              Expanded(
-                child: Text('COLUMN widget', textAlign: TextAlign.center),
-              ),
-              Expanded(
-                child: Text('COLUMN widget', textAlign: TextAlign.center),
-              ),
-              Expanded(
-                child: Text('COLUMN widget', textAlign: TextAlign.center),
-              ),
-              Expanded(
-                child: Text('COLUMN widget', textAlign: TextAlign.center),
-              ),
-            ],
-          ),
+            Icon(
+                Icons.local_movies,
+              color: Colors.lightBlue,
+              size: 200,
 
-          Row(
-            children: [
-              Stack(
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    width: 90,
-                    height: 90,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    width: 80,
-                    height: 80,
-                    color: Colors.blue,
-                    child: const Text('STACK widget'),
-                  ),
-                ],
+
               ),
-            ],
 
-          ),
+          ],
 
-        ],
+      ),
+
       ),
 
 
       ),
-
-
-
-
       ),
-    ),
+
 );
 
 
